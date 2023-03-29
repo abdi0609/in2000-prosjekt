270a766c38764e33491e0ebeb754ac7396286d37
Oskar Haukebøe <oskah@ulrik.uio.no>
2023-03-29 16:05:15 +0200

    Minimum requred API (#21)
    Updated minimum required API from 23 to 26, and some basic code cleanup.
    
    Co-authored-by: Oskar Haukebøe <oskah@uio.no>

--- app/src/main/java/com/example/stromkalkulator/data/models/UserModelDummy.kt
+++
@@ -1,4 +0,0 @@
-package com.example.stromkalkulator.data.models
-
-class UserModelDummy {
-}
--- app/src/main/java/com/example/stromkalkulator/ui/screens/DetailedScreen.kt
+++ app/src/main/java/com/example/stromkalkulator/ui/screens/DetailedScreen.kt
@@ -1,7 +1,5 @@
 package com.example.stromkalkulator.ui.screens
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,22 +9,19 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.stromkalkulator.data.models.Day
-import com.example.stromkalkulator.data.models.HourPrice
-import com.example.stromkalkulator.data.models.Week
 import com.example.stromkalkulator.data.repositories.ElectricityPrice
 
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DetailedView(innerPadding: PaddingValues) {
 
     val electricityPrice = ElectricityPrice()
-    val hours = produceState<Day>(
+    val hours = produceState(
         initialValue = Day(emptyList()),
         producer = { value = electricityPrice.getTomorrow() })
 
     LazyColumn(
-        modifier = Modifier.padding(PaddingValues())
+        modifier = Modifier.padding(innerPadding)
     ) {
         items(hours.value.hours){
             Column(
@@ -48,50 +43,5 @@ fun DetailedView(innerPadding: PaddingValues) {
             }
         }
     }
-
-
- /*
-
-    //Not working
-    val electricityPrice = ElectricityPrice()
-    val hours = produceState<Week>(
-        initialValue = Week(emptyList<Day>().toMutableList()),
-        producer = { value = electricityPrice.getWeek() }
-    )
-
-    LazyColumn(modifier = Modifier.padding(PaddingValues())) {
-        items(hours.value.days) { hourPrices ->
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp, top = 30.dp)
-            ) {
-                var i = 0
-                hourPrices.hours.forEach { _ ->
-                    Text(text = "Stromtime ${i}")
-                    i++
-                /*
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Text(text = it.NOK_per_kWh.toString())
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = it.EUR_per_kWh.toString())
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = it.EXR.toString())
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = it.time_start.toString())
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = it.time_end.toString())
-
-                     */
-                }
-            }
-        }
-    }
-
-
-
-
-  */
-
 }
 
