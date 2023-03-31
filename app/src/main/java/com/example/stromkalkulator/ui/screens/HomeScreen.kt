989b199e58fcfbc78edc3cffe612bd1062f5b141
Marcus Rand Ikdal <marcusri@ulrik.uio.no>
2023-03-31 13:43:17 +0200

    25 fremstille prisgrad i boblen farge (#29)
    Made bubble better:
    -Composable function for current price bubble
    -Custom colors for bubble
    -Made bubble smaller
    -2 decimals

--- app/src/main/java/com/example/stromkalkulator/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/stromkalkulator/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.stromkalkulator.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
@@ -8,15 +9,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.stromkalkulator.data.repositories.ElectricityPrice
+import com.example.stromkalkulator.ui.theme.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 @Composable
 fun HomeScreen(paddingValue: PaddingValues) {
     val electricityPrice = ElectricityPrice()
     val current = produceState<String>(
-        initialValue = "hei",
+        initialValue = "0.0",
         producer = { value = electricityPrice.getCurrent() })
 
     Column (
@@ -27,17 +33,7 @@ fun HomeScreen(paddingValue: PaddingValues) {
         verticalArrangement = Arrangement.spacedBy(30.dp)
     ) {
         Spacer(modifier = Modifier.size(40.dp))
-        Card (
-            modifier = Modifier.size(250.dp, 250.dp),
-            shape = CircleShape,
-        ) {
-            Box (
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(text = "${current.value} kr/kwh")
-            }
-        }
+        CurrentPriceBubble(priceString = current.value)
 
         Card(modifier = Modifier.size(300.dp, 300.dp)) {
             Box(
@@ -50,7 +46,47 @@ fun HomeScreen(paddingValue: PaddingValues) {
     }
 }
 
+// Card that shows current price
+@Composable
+fun CurrentPriceBubble(priceString: String) {
+    val price = priceString.toDouble()
+    val fontSize = 25.sp
 
+    // 2 decimals
+    val df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.HALF_UP
+
+    val textColor =
+        // if low price
+        if (price < 1.2) GreenTextContrast
+        // average price
+        else if (price < 2) YellowTextContrast
+        // high price
+        else RedTextContrast
+
+    val backgroundColor =
+        // if low price
+        if (price < 1.2) Green
+        // average price
+        else if (price < 2) Yellow
+        // high price
+        else Red
+
+    Card (
+        modifier = Modifier.size(200.dp, 200.dp),
+        shape = CircleShape,
+    ) {
+        Box (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(backgroundColor),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = "${df.format(price)} kr/kwh", color = textColor, fontSize = fontSize,
+                fontWeight = FontWeight.Bold)
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
