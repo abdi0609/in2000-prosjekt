71e86ec414d480134e9195a7ec1478b518137344
Tor Carlos Høydahl Ohme <torcho@uio.no>
2023-04-04 17:17:28 +0200

    Fikset API tomorrow() og week() (#32)
    Co-authored-by: magokstad <magokstad@gmail.com>

--- app/src/main/java/com/example/stromkalkulator/data/repositories/ElectricityPrice.kt
+++ app/src/main/java/com/example/stromkalkulator/data/repositories/ElectricityPrice.kt
@@ -5,62 +5,46 @@ import com.example.stromkalkulator.data.models.electricity.HourPrice
 import com.example.stromkalkulator.data.models.electricity.Week
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
+import java.util.Calendar
 
 
 class ElectricityPrice (private val client: HttpClient) {
+    // Denne er visst ikke brukt????
+    /*
     private val daysInMonth = hashMapOf(
         "01" to 31, "02" to 28, "03" to 31,
         "04" to 30, "05" to 31, "06" to 30,
         "07" to 31, "08" to 31, "09" to 30,
         "10" to 31, "11" to 30, "12" to 31
     )
+    */
     suspend fun getTomorrow(): Day {
         return try {
             val calendar = Calendar.getInstance()
-            var dayInt = (calendar.get(Calendar.DAY_OF_MONTH) + 1)
-            var dayString = dayInt.toString()
-            var monthInt = (calendar.get(Calendar.MONTH) + 1)
-            var monthString = monthInt.toString()
-            if (monthString.length == 1) { monthString = "0$monthString" }
-            var yearInt = calendar.get(Calendar.YEAR)
-            var yearString = yearInt.toString()
-            val currentDateTime = LocalDateTime.now()
-            val hour = currentDateTime.format(DateTimeFormatter.ofPattern("HH"))
-            if (hour.toInt() < 14) { return Day(emptyList()) }
+            calendar.add(Calendar.MONTH, 1)
+            if (calendar.get(Calendar.HOUR_OF_DAY) < 14) { return Day(emptyList()) }
 
-            if ((dayInt - 1) == daysInMonth[monthString]) {
-                dayInt = 1
-                dayString = dayInt.toString()
-                monthInt++
-                monthString = monthInt.toString()
-                if (monthInt == 13) {
-                    monthInt = 1
-                    monthString = monthInt.toString()
-                    yearInt++
-                    yearString = yearInt.toString()
-                }
-            }
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+            // Denne er visst aldri brukt heller
+            //val tomorrow = calendar.time
+
+            var dayString = calendar.get(Calendar.DAY_OF_MONTH).toString()
+            var monthString = calendar.get(Calendar.MONTH).toString()
             if (dayString.length == 1) { dayString = "0$dayString" }
             if (monthString.length == 1) { monthString = "0$monthString" }
 
 
-            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" + yearString + "/" + monthString + "-" + dayString + "_NO5.json"
+            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" +
+                    calendar.get(Calendar.YEAR).toString() + "/" + monthString + "-" + dayString + "_NO5.json"
             println(url)
 
-            val dayList: List<HourPrice> = client.get(url).body<List<HourPrice>>()
+            val dayList: List<HourPrice> = client.get(url).body()
             Day(dayList)
         } catch (_: Exception) {
             Day(emptyList())
         }
     }
-
 
     suspend fun getToday(): Day {
         return try {
@@ -69,16 +53,16 @@ class ElectricityPrice (private val client: HttpClient) {
 
             //get(month) gir dagen før. Vet ikke hvorfor
             var month = (calendar.get(Calendar.MONTH) + 1).toString()
-            if (month.length == 1) { month = "0$month" }
             var day = calendar.get(Calendar.DAY_OF_MONTH).toString()
+            if (month.length == 1) { month = "0$month" }
             if (day.length == 1) { day = "0$day" }
 
-            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" + year + "/" + month + "-" + day + "_NO5.json"
+            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" + year +
+                    "/" + month + "-" + day + "_NO5.json"
             println(url)
-            val dayList: List<HourPrice> = client.get(url).body<List<HourPrice>>()
+            val dayList: List<HourPrice> = client.get(url).body()
             Day(dayList)
-        } catch (_: Exception) {
-             Day(emptyList()) }
+        } catch (_: Exception) { Day(emptyList()) }
     }
 
     //converts date-string to HOUR
@@ -97,36 +81,18 @@ class ElectricityPrice (private val client: HttpClient) {
     }
 
     suspend fun getWeek(): Week {
-        val calendar = Calendar.getInstance()
-        var dayInt = (calendar.get(Calendar.DAY_OF_MONTH) + 1)
-        var dayString = dayInt.toString()
-        var monthInt = (calendar.get(Calendar.MONTH) + 1)
-        var monthString = monthInt.toString()
-        var yearInt = calendar.get(Calendar.YEAR)
-        var yearString = yearInt.toString()
-
         val urlList = mutableListOf<String>()
-        for (i in 0..5) {
-            dayInt--
-            dayString = dayInt.toString()
-            if (dayInt == 0) {
-                monthInt--
-                monthString = monthInt.toString()
-                if (monthInt == 0) {
-                    monthInt = 12
-                    monthString = monthInt.toString()
-                    yearInt--
-                    yearString = yearInt.toString()
+        val calendar = Calendar.getInstance()
+        for (i in 0..6) {
+            var month = (calendar.get(Calendar.MONTH) + 1).toString()
+            var day = calendar.get(Calendar.DAY_OF_MONTH).toString()
+            if (month.length == 1) { month = "0$month" }
+            if (day.length == 1) { day = "0$day" }
 
-                }
-                if (monthString.length == 1) { monthString = "0$monthString" }
-                dayInt = daysInMonth[monthString]!!
-            }
-            if (dayString.length == 1) { dayString = "0$dayString" }
-            if (monthString.length == 1) { monthString = "0$monthString" }
-
-            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" + yearString + "/" + monthString + "-" + dayString + "_NO5.json"
+            val url = "https://www.hvakosterstrommen.no/api/v1/prices/" +
+                    calendar.get(Calendar.YEAR) + "/" + month + "-" + day + "_NO5.json"
             urlList.add(url)
+            calendar.add(Calendar.DAY_OF_YEAR, -1)
         }
 
         val hourPriceList = emptyList<Day>().toMutableList()
